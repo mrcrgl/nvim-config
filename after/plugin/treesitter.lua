a4ed0d8d71a776a675f4db308895bba47f1483d1
Marc Riegel <mail@mrcrgl.de>
2023-12-28 23:33:32 +0100

    changed installation mode of treesitter to git

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,3 +1,5 @@
+require("nvim-treesitter.install").prefer_git = true
+
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "typescript", "javascript", "c", "lua", "rust", "vim", "vimdoc", "query" },
